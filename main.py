b22f5b384ee7a72a709cd7d15219c69c6a52c24b
Regnier251 <regnier251hd@gmail.com>
2025-08-05 13:50:46 -0600

    Juego cruzar la calle, Turtle graphic python

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle cross")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up,"w")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_left()
+
+    #Collision cars
+    for car in car_manager.all_cars:
+       if car.distance(player)<21:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #successful cross
+    if player.finish_line():
+       player.go_to_start()
+       car_manager.level_up()
+       scoreboard.increase_level()
+
+
+screen.exitonclick()
